099683d0370ac1fc1517283ada08e3db9b5b5b41
bala-amypo <bala@amypo.in>
2025-12-19 09:46:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/LoanRequestRepository.java
+++ src/main/java/com/example/demo/repository/LoanRequestRepository.java
@@ -2,6 +2,10 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-public interface LoanRequestRepository extends JpaRepository<{
+
+import com.example.demo.entity.LoanRequest;
+
+@Repository
+public interface LoanRequestRepository extends JpaRepository<LoanRequest,Integer>{
     
 }
--- src/main/java/com/example/demo/repository/RiskAssessmentLogRepository.java
+++ src/main/java/com/example/demo/repository/RiskAssessmentLogRepository.java
@@ -1,4 +1,5 @@
 package com.example.demo.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 public interface RiskAssessmentLogRepository{
     
 }
