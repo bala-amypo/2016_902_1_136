3306c3665cc83168723d4c5693817bc6a56b9d34
bala-amypo <bala@amypo.in>
2025-12-19 09:43:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/LoanRequestRepository.java
+++ src/main/java/com/example/demo/repository/LoanRequestRepository.java
@@ -1,4 +1,7 @@
 package com.example.demo.repository;
-public interface LoanRequestRepository{
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+public interface LoanRequestRepository extends JpaRepository<{
     
 }
