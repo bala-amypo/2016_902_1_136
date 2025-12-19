769fc5b09cbd0000b4eebbbeaab25d20837c69cc
bala-amypo <bala@amypo.in>
2025-12-19 09:31:05 +0000

    User commit

--- src/main/java/com/example/demo/repository/EligibilityResultRepository.java
+++ src/main/java/com/example/demo/repository/EligibilityResultRepository.java
@@ -3,7 +3,7 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.
-public interface EligibilityResultRepository{
+import com.example.demo.entity.EligibilityResult;
+public interface EligibilityResultRepository extends JpaRepository<{
     
 }
