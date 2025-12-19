80c6f42a904607ec8bb239b8d815b1985501bdb0
bala-amypo <bala@amypo.in>
2025-12-19 06:39:58 +0000

    User commit

--- src/main/java/com/example/demo/entity/EligibilityResult.java
+++ src/main/java/com/example/demo/entity/EligibilityResult.java
@@ -1,8 +1,12 @@
 package com.example.demo.entity;
 import java.Time.*;
 import jakarta.persisitence.*;
+
+@Entity
 public class EligibilityResult{
     @OneToOneLoanRequest
+    @Id;
+    @GeneratedValue(strategy=GeneratitonType.IDENTITY)
     private Long id;
     private String loanRequest;
     private Boolean isEligible;
@@ -10,5 +14,9 @@ public class EligibilityResult{
     private Double estimatedEmi;
     private String riskLevel;
     private String rejectionReason;
-    private 
+    private Timestamp calculatedAt;
+
+    public Long getId(){
+
+    }
 }
