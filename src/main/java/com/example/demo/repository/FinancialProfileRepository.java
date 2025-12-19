ecaa2fddec62a3065c1d560f063591c2945c0305
bala-amypo <bala@amypo.in>
2025-12-19 09:40:04 +0000

    User commit

--- src/main/java/com/example/demo/repository/FinancialProfileRepository.java
+++ src/main/java/com/example/demo/repository/FinancialProfileRepository.java
@@ -1,4 +1,8 @@
 package com.example.demo.repository;
-public interface FinancialProfileRepository{
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.example.demo.entity.FinancialProfile;
+@Repository
+public interface FinancialProfileRepository extends JpaRepository<FinancialProfile,Integer>{
+
 }
