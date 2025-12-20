f8dbdb2ed8196c90776ced626db9865715407acd
bala-amypo <bala@amypo.in>
2025-12-20 17:42:25 +0000

    User commit

--- src/main/java/com/example/demo/controller/EligibilityController.java
+++ src/main/java/com/example/demo/controller/EligibilityController.java
@@ -1,24 +1,13 @@
 package com.example.demo.controller;
 
-import com.example.demo.entity.LoanEligibility;
-import com.example.demo.repository.LoanEligibilityRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/eligibility")
 public class EligibilityController {
 
-    @Autowired
-    private LoanEligibilityRepository eligibilityRepository;
-
-    @PostMapping
-    public LoanEligibility save(@RequestBody LoanEligibility eligibility) {
-        return eligibilityRepository.save(eligibility);
-    }
-
-    @GetMapping("/{id}")
-    public LoanEligibility get(@PathVariable Long id) {
-        return eligibilityRepository.findById(id).orElse(null);
+    @GetMapping("/{loanId}")
+    public String checkEligibility(@PathVariable Long loanId) {
+        return "Eligibility check completed for loanId: " + loanId;
     }
 }
