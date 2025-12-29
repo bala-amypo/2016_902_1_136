c6f5abc61b8dfcb37cb0f764e6828fc87726e616
bala-amypo <bala@amypo.in>
2025-12-29 06:20:44 +0000

    User commit

--- src/main/java/com/example/demo/service/EligibilityService.java
+++ src/main/java/com/example/demo/service/EligibilityService.java
@@ -2,6 +2,7 @@ package com.example.demo.service;
 
 import com.example.demo.entity.EligibilityResult;
 
+@Service
 public interface EligibilityService {
     EligibilityResult evaluateEligibility(Long loanRequestId);
     EligibilityResult getByLoanRequestId(Long loanRequestId);
